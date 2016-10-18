1b1e9d5a1427ee1f13f064f0d023711f9319bbe2
Jessica Brisson <jbrisson2@studentmail.nait.ca>
2016-10-18 16:08:13 -0600

    setup of profile datasets to be used to carry webpage data

---
+++ ChinookSample/ChinookSystem/Security/UnregisteredUserProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnregisteredUserType
+    {
+        Undefined, Employee, Customer
+    }
+    class UnregisteredUserProfile
+    {
+        public string UserId { get; set; } //generated when user added
+        public string UserName { get; set; } //collected from form
+        public string FirstName { get; set; } //comes from user table
+        public string LastName { get; set; } //comes from user table
+        public string Email { get; set; } //collected from form
+        public UnregisteredUserType UserType { get; set; }
+
+
+    }
+}
---
+++ ChinookSample/ChinookSystem/Security/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public class UserProfile
+    {
+        public string UserId { get; set; } //from AspNet User table
+        public string UserName { get; set; }
+        public int? EmployeeId { get; set; }//from AspNet User table
+        public int? CustomerId { get; set; } //from AspNet User table
+        public string FirstName { get; set; } //from Employee or Customer table
+        public string LastName { get; set; }//from Employee or Customer table
+        public string Email { get; set; } // from AspNet User table
+        public string EmailConfirm { get; set; } //from AspNet User table
+        public IEnumerable<string> RoleMemberships { get; set; }
+
+
+    }
+}
